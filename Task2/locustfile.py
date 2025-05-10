99f377a6ecfa864a6bdda697dfd49ad9cad5774b
i.gulomqodirov <i.gulomqodirov@kapitalbank.uz>
2025-05-10 14:01:20 +0300

    add: task2 solution with the screenshots

---
+++ Task2/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, between, task
+
+class WebsiteUser(HttpUser):
+ wait_time = between(1, 5)
+ host = "http://192.168.49.2:30090"
+
+ @task
+ def index(self):
+     self.client.get("/")
